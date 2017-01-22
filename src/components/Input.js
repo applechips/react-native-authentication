70d9465e7cd0f46151aece50d045e8fc8d162ee4
Nicole <nhswong@gmail.com>
2017-01-21 23:13:17 -0800

    add TextInput & create  Input.js component

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = () => {
+  return (
+    <View>
+      <Text>{label}</Text>
+    </View>
+  );
+};
+
+export { Input }
