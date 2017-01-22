7cc42aa8030a4aee0aa9fe330c27cedd5bf7d32e
Nicole <nhswong@gmail.com>
2017-01-21 22:33:02 -0800

    add src file to contain components file

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { App Registry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/app.js';
 
-AppRegistry.registerComponent('Authentication', () => Authentication);
+AppRegistry.registerComponent('Authentication', () => App);
