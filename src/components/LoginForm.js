8fc3cb8a2a5ab1e48d12b2c71a173c10933653a9
Nicole <nhswong@gmail.com>
2017-01-21 23:00:50 -0800

    add TextInput field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'React';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <TextInput style={{ height: 20, width: 100 }}/>
+        </CardSection>
+        <CardSection>
+          <TextInput style={{ height: 20, width: 100 }}/>
+        </CardSection>
         <CardSection>
           <Button>
             Log In
