ac68e8939517f803a709f2503717f3c98d38ffac
Nicole <nhswong@gmail.com>
2017-01-23 16:43:35 -0800

    remove autocorrect in email input, add placeholder prop

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,18 +2,19 @@ import React, { Component } from 'React';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+          placeholder="user@gmail.com"
           label="Email"
-          value={this.state.text}
-          onChangeText={ text => this.setState({ text })} />
+          value={this.state.email}
+          onChangeText={ email => this.setState({ email })} />
         </CardSection>
-        <CardSection>
-        </CardSection>
+
+        <CardSection />
 
         <CardSection>
           <Button>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText}) => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        placeholder={placeholder}
+        autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
-        style={{ height: 20, width: 100 }}
       />
     </View>
   );
@@ -19,7 +20,7 @@ const Input = ({ label, value, onChangeText}) => {
 
 const styles = {
   inputStyle: {
-    color: '#0000',
+    color: '#000',
     paddingRight: 5,
     paddingLeft: 5,
     fontSize: 18,
