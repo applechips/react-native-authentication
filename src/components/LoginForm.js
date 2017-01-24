a8e30c356d940dc14fda8dda1b968592e9170937
Nicole <nhswong@gmail.com>
2017-01-23 16:33:41 -0800

    add styling to text input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,12 +8,13 @@ class LoginForm extends Component {
       <Card>
         <CardSection>
           <Input
+          label="Email"
           value={this.state.text}
           onChangeText={ text => this.setState({ text })} />
         </CardSection>
         <CardSection>
         </CardSection>
-        
+
         <CardSection>
           <Button>
             Log In
